27c04d621ba8f7cb788efdd48cbc30d94efd2c74
Sameel <sameel.m.arif@gmail.com>
2024-12-01 11:39:04 -0500

    add packages response schema

---
+++ models/llm_models/packages.go
@@ -0,0 +1,24 @@
+package llm_models
+
+import (
+	"github.com/init-stuff/init/models"
+	"github.com/openai/openai-go"
+)
+
+var (
+	packagesResponseSchema = models.GenerateSchema[Packages]()
+
+	PackagesResponseSchemaParam = openai.ResponseFormatJSONSchemaJSONSchemaParam{
+		Name:        openai.F("packages"),
+		Description: openai.F("A list of python packages required to run the code."),
+		Schema:      openai.F(packagesResponseSchema),
+		Strict:      openai.F(true),
+	}
+)
+
+type Packages = []Package
+
+type Package struct {
+	Name    string `json:"name" jsonschema_description:"The name of the package."`
+	Version string `json:"version" jsonschema_description:"The version of the package. Always set this to \"latest\" unless a specific version is required."`
+}
---
+++ models/schemautil.go
@@ -0,0 +1,13 @@
+package models
+
+import "github.com/invopop/jsonschema"
+
+func GenerateSchema[T any]() interface{} {
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+	schema := reflector.Reflect(v)
+	return schema
+}
